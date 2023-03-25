db504c848e956a9330e6da1abef7b7f1a3e7b3bd
Julian <julian.righ.sampedro@gmail.com>
2023-03-25 19:43:02 +0100

    Add files via upload

---
+++ Normal table/Normal table.R
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------
+# Generate a table of Standard Normal distribution: area under curve
+#-------------------------------------------------------------------
+
+setwd("C:/Users/julia/OneDrive/Desktop/github/21. Normaltable")
+library(xtable)
+
+u=seq(0,3.09,by=0.01)
+p=pnorm(u)
+m=matrix(p,ncol=10,byrow=TRUE)
+colnames(m) = seq(0, 0.09, by = 0.01)
+rownames(m) = seq(0,3, by = 0.1)
+
+# export the results in LaTex document
+print(xtable(m, type = "latex"), file = "tables.tex")
+
+#----
+# end
+#----
