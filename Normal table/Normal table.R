80d3f7e46342a62535bb0551a8cb85e4993b1f66
Julian <julian.righ.sampedro@gmail.com>
2023-03-26 09:54:40 +0200

    Add files via upload

--- Normal table/Normal table.R
+++ Normal table/Normal table.R
@@ -7,12 +7,12 @@ library(xtable)
 
 u=seq(0,3.09,by=0.01)
 p=pnorm(u)
-m=matrix(p,ncol=10,byrow=TRUE)
+m=round(matrix(p,ncol=10,byrow=TRUE),4)
 colnames(m) = seq(0, 0.09, by = 0.01)
 rownames(m) = seq(0,3, by = 0.1)
 
 # export the results in LaTex document
-print(xtable(m, type = "latex"), file = "tables.tex")
+print(xtable(m, type = "latex", digits=4), file = "tables.tex")
 
 #----
 # end
