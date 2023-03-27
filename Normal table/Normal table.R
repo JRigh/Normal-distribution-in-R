a6226a8790642fd1cee5d4fc59ee162ded874ef9
Julian <julian.righ.sampedro@gmail.com>
2023-03-27 19:13:18 +0200

    Add files via upload

--- Normal table/Normal table.R
+++ Normal table/Normal table.R
@@ -5,14 +5,14 @@
 setwd("C:/Users/julia/OneDrive/Desktop/github/21. Normaltable")
 library(xtable)
 
-u=seq(0,3.09,by=0.01)
-p=pnorm(u)
-m=round(matrix(p,ncol=10,byrow=TRUE),4)
-colnames(m) = seq(0, 0.09, by = 0.01)
-rownames(m) = seq(0,3, by = 0.1)
+u = seq(0,3.09,by=0.01)
+p = pnorm(u)
+res = round(matrix(p,ncol=10,byrow=TRUE),4)
+colnames(res) = seq(0, 0.09, by = 0.01)
+rownames(res) = seq(0,3, by = 0.1)
 
 # export the results in LaTex document
-print(xtable(m, type = "latex", digits=4), file = "tables.tex")
+print(xtable(res, type = "latex", digits=4), file = "tables.tex")
 
 #----
 # end
